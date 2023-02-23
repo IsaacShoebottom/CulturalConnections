58db311540e29353891070984740d2680b8f236c
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-02-22 23:05:50 -0400

    This fixes loading fonts, and also fixes bootstrap not loading properly.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<h1
+style="font-family: 'Avenir-Book'"
+>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div style="">
