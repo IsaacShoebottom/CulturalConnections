7c2a9b3022e1c6a2dce7347a393eb2a8f992c51a
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-02-22 21:50:25 -0400

    Add skeleton of project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div style="">
+    <div><a href="recipes">Recipes</a> </div>
+</div>
+
---
+++ src/routes/recipes/add/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Add recipes here</h1>
